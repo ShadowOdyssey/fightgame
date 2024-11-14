80e721ad0308844ec4c17710246f75aa8fa632bd
ShadowOdyssey <ricardosolenoides@gmail.com>
2024-11-13 23:11:33 -0300

    Added AI difficulty and personality
    Read the code to understand

--- Assets/Scripts/Old/MarcusAi.cs
+++ Assets/Scripts/Old/MarcusAi.cs
@@ -11,16 +11,23 @@ public class MarcusAI : MonoBehaviour
     public Animator animator;
 
     public int health = 100;
+    public int enemyDifficulty = 0; // 0 = easy 1 = moderate 2 = normal 3 = hard
+    public int randomMaxValue = 100;
     public float attackRange = 13f;
     public float moveSpeed = 2f;
+    public float randomizeTimer = 0f;
 
     private Vector3 originalPosition; // Save original position to reset after getting up
+    private int actualRandomValue = 0;
     private int hitCount = 0; // Track the number of times Marcus is hit
     private float distanceToTarget = 0f;
+    private bool successRandom = false; // The success is based in the difficulty level
+    private bool isResetRandom = false;
     private bool isWalking = false;
     private bool isAttacking = false;
     private bool canFight = false;
     private bool checkDamage = false;
+    private bool canRandomize = false;
     private bool changedAnimDirectionToForward = false;
     private bool changedAnimDirectionToBackward = false;
 
@@ -31,17 +38,172 @@ public class MarcusAI : MonoBehaviour
 
     private void Update()
     {
-        if (canFight == false && roundSystem.roundStarted == true) // Only can execute commands in FixedUpdate if round started, but...
+        if (canFight == false && roundSystem.roundStarted == true && successRandom == false) // Only can execute commands in FixedUpdate if round started, but...
         {
             Debug.Log("Round Started");
 
-            canFight = true;
+            canRandomize = true; // Round started, so activate randomizer
+            canFight = true; // Round started, so can fight
         }
 
-        if (canFight == true && roundSystem.roundStarted == false) // If round not started and is 2nd or 3rd round, load Idle animation till round start again!
+        if (canFight == true && roundSystem.roundStarted == false) // If round not started and is 2nd or 3rd round, load Idle animation till round start again! - Felipe
         {
-            Idle();
-            canFight = false;
+            canRandomize = false; // Round finished, stop to randomize - Felipe
+            randomizeTimer = 0f; // Reset randomizer time when next round to start if to have a new round yet
+            canFight = false; // Round finished, stop to fight - Felipe
+            isWalking = false;
+            changedAnimDirectionToBackward = false;
+            changedAnimDirectionToForward = false;
+            Idle(); // Round finished, trigger Idle animation - We can change it later to defeat animation or victory animation on each round based in remaning life - Felipe
+        }
+
+        if (canRandomize == true)
+        {
+            randomizeTimer = randomizeTimer + Time.deltaTime;
+
+            if (randomizeTimer >= 3f)
+            {
+                actualRandomValue = Random.Range(1, randomMaxValue);
+
+                switch (enemyDifficulty) // Check sucess in the random number generated
+                {
+                    case 0: // Less agressive and less defensive
+
+                        if (actualRandomValue <= 1 && actualRandomValue <= 80)
+                        {
+                            successRandom = false; // Continue to do what is doing
+                        }
+
+                        if (actualRandomValue >= 81 &&  actualRandomValue <= 100)
+                        {
+                            successRandom = true; // No agression and no defense - Call for more Idle and stop to move in the middle of the combate and dont attack
+                        }
+
+                        break;
+                    
+                    case 1: // less agressive and more defensive
+
+                        if (actualRandomValue <= 1 && actualRandomValue <= 40)
+                        {
+                            successRandom = false; // Continue to do what is doing
+                        }
+
+                        if (actualRandomValue >= 41 && actualRandomValue <= 100)
+                        {
+                            successRandom = true; // Move more far from player or use defensive skills if possible
+                        }
+
+                        break;
+                    
+                    case 2: // More agressive and less defensive
+
+                        if (actualRandomValue <= 1 && actualRandomValue <= 80)
+                        {
+                            successRandom = false; // Continue to do what is doing
+                        }
+
+                        if (actualRandomValue >= 81 && actualRandomValue <= 100)
+                        {
+                            successRandom = true; // Move more near from player or use aggressive skills if possible
+                        }
+
+                        break;
+                    
+                    case 3: // More agressive and more defensive
+
+                        if (actualRandomValue <= 1 && actualRandomValue <= 80)
+                        {
+                            successRandom = false; // Continue to do what is doing
+                        }
+
+                        if (actualRandomValue >= 81 && actualRandomValue <= 100)
+                        {
+                            successRandom = true; // Move more near from player and use aggressive skills if player is far or move more far from player or use defensive skills if possible
+                        }
+
+                        break;
+                }
+
+                if (isWalking == true && successRandom == true) // Apply the success in random number based in the level difficulty
+                {
+                    switch (enemyDifficulty)
+                    {
+                        case 0: // Stop to move AI and let it more vulnerable to attacks
+
+                            if (isAttacking == false)
+                            {
+                                canFight = false;
+                                isWalking = false;
+                                changedAnimDirectionToBackward = false;
+                                changedAnimDirectionToForward = false;
+                                Idle();
+                            }
+
+                            break;
+
+                        case 1: // Follow less the player and use more defensife skills
+                            
+                            if (changedAnimDirectionToForward == true && isAttacking == false)
+                            {
+                                changedAnimDirectionToForward = false;
+                                changedAnimDirectionToBackward = true;
+                            }
+
+                            break;
+
+                        case 2: // Follow more the player and use more aggressive skills
+
+                            if (changedAnimDirectionToBackward == true && isAttacking == false)
+                            {
+                                changedAnimDirectionToForward = true;
+                                changedAnimDirectionToBackward = false;
+                            }
+
+                            break;
+
+                        case 3: // Follow player if far and move far if near player
+
+                            if (isAttacking == false)
+                            {
+                                if (distanceToTarget > attackRange)
+                                {
+                                    changedAnimDirectionToForward = true;
+                                    changedAnimDirectionToBackward = false;
+                                }
+
+                                if (distanceToTarget <= attackRange)
+                                {
+                                    changedAnimDirectionToForward = false;
+                                    changedAnimDirectionToBackward = true;
+                                }
+                            }
+
+                            break;
+                    }
+                }
+
+                isResetRandom = true;
+                randomizeTimer = 0f;
+                canRandomize = false;
+            }
+        }
+
+        if (isResetRandom == true)
+        {
+            randomizeTimer = randomizeTimer + Time.deltaTime;
+
+            if (randomizeTimer > 2f)
+            {
+                randomizeTimer = 0f;
+
+                if (canFight == false)
+                {
+                    canFight = true;
+                }
+
+                canRandomize = true;
+                isResetRandom = false;
+            }
         }
     }
 
@@ -89,14 +251,14 @@ public class MarcusAI : MonoBehaviour
     {
         StartWalkAnimation();
 
-        if (isWalking == true)
+        if (isWalking == true && enemyDifficulty > 0 || isWalking == true && enemyDifficulty == 0 && successRandom == false)
         {
-            if (changedAnimDirectionToForward == true)
+            if (changedAnimDirectionToForward == true || successRandom == true && enemyDifficulty == 2 || successRandom == true && enemyDifficulty == 3)
             {
                 transform.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveSpeed * Time.deltaTime);
             }
             
-            if (changedAnimDirectionToBackward == true)
+            if (changedAnimDirectionToBackward == true || successRandom == true && enemyDifficulty == 1 || successRandom == true && enemyDifficulty == 3)
             {
                 transform.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed * Time.deltaTime);
             }
@@ -159,7 +321,9 @@ public class MarcusAI : MonoBehaviour
 
     private void StartWalkAnimation()
     {
-        if (animator.GetBool("isForward") == false && changedAnimDirectionToForward == true) // Prevents to execute animation call many times, this way we only call 1 time the correct animation
+        if (animator.GetBool("isForward") == false && changedAnimDirectionToForward == true ||
+            enemyDifficulty == 0 && successRandom == false && animator.GetBool("isForward") == false && changedAnimDirectionToForward == true)
+        // Prevents to execute animation call many times, this way we only call 1 time the correct animation
         {
             animator.SetBool("isIdle", false); // Values in parameters should be low case in the first letter because is variable name - Felipe
             animator.SetBool("isForward", true); // Values in parameters should be low case in the first letter because is variable name - Felipe
@@ -168,7 +332,9 @@ public class MarcusAI : MonoBehaviour
             isWalking = true;
         }
 
-        if (animator.GetBool("isBackward") == false && changedAnimDirectionToBackward == true) // Prevents to execute animation call many times, this way we only call 1 time the correct animation
+        if (animator.GetBool("isBackward") == false && changedAnimDirectionToBackward == true ||
+            enemyDifficulty == 0 && successRandom == false && animator.GetBool("isBackward") == false && changedAnimDirectionToBackward == true)
+            // Prevents to execute animation call many times, this way we only call 1 time the correct animation
         {
             animator.SetBool("isIdle", false); // Values in parameters should be low case in the first letter because is variable name - Felipe
             animator.SetBool("isForward", false); // Values in parameters should be low case in the first letter because is variable name - Felipe
