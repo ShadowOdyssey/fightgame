55d918eac03cafb22b3312e5ceb944c66fc72458
ShadowOdyssey <ricardosolenoides@gmail.com>
2024-11-15 20:13:17 -0300

    Fixed minor issue

--- Assets/Scripts/Fixed/PlayerSystem.cs
+++ Assets/Scripts/Fixed/PlayerSystem.cs
@@ -161,6 +161,7 @@ public class PlayerSystem : MonoBehaviour
 
             if (damageTime > 1f)
             {
+                damageTime = 0f;
                 checkDamage = false;
             }
         }
