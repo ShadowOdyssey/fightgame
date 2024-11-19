0856323242ff2955281515927aa0dca1346601f4
ShadowOdyssey <ricardosolenoides@gmail.com>
2024-11-19 19:44:50 -0300

    Lobby Update

--- Assets/Scripts/Fixed/LobbyPlayerSystem.cs
+++ Assets/Scripts/Fixed/LobbyPlayerSystem.cs
@@ -11,9 +11,46 @@ public class LobbyPlayerSystem : MonoBehaviour
     public TextMeshProUGUI buttonFightText;
 
     private LobbyManager lobbySystem;
+    private int actualProfile = 0;
 
     private void Start()
     {
         lobbySystem = GameObject.Find("Lobby Manager").GetComponent<LobbyManager>();
+        LoadProfile();
+    }
+
+    public void LoadProfile()
+    {
+        switch (actualProfile)
+        {
+            case 1: imageProfile.sprite = lobbySystem.gabriellaProfile; break;
+            case 2: imageProfile.sprite = lobbySystem.marcusProfile; break;
+            case 3: imageProfile.sprite = lobbySystem.selenaProfile; break;
+            case 4: imageProfile.sprite = lobbySystem.bryanProfile; break;
+            case 5: imageProfile.sprite = lobbySystem.nunProfile; break;
+            case 6: imageProfile.sprite = lobbySystem.oliverProfile; break;
+            case 7: imageProfile.sprite = lobbySystem.orionProfile; break;
+            case 8: imageProfile.sprite = lobbySystem.ariaProfile; break;
+        }
+    }
+
+    public void UpdateName(string newName)
+    {
+        playerName.text = newName;
+    }
+
+    public void UpdateWins(string actualWins)
+    {
+        totalWins.text = actualWins;
+    }
+
+    public void UpdateStatus(string newStatus)
+    {
+        buttonFightText.text = newStatus;
+    }
+
+    public void UpdateProfile(int newProfile)
+    {
+        actualProfile = newProfile;
     }
 }
