b4a0ee1b0bb57d9304b14497908fde786221ae78
ShadowOdyssey <ricardosolenoides@gmail.com>
2024-11-22 23:28:52 -0300

    Multiplayer Update

--- Assets/Scripts/Fixed/EnemySystem.cs
+++ Assets/Scripts/Fixed/EnemySystem.cs
@@ -1010,7 +1010,7 @@ public class EnemySystem : MonoBehaviour
 
             //Debug.Log("Player moved to right");
 
-            moveDirection = 1; // Setup new direction only once before to apply new position - 
+            moveDirection = -1; // Setup new direction only once before to apply new position - 
             enemyAnimator.SetBool("isForward", true); // Values in parameters should be low case in the first letter because is variable name - 
             enemyAnimator.SetBool("isIdle", false); // Values in parameters should be low case in the first letter because is variable name - 
             enemyAnimator.SetBool("isBackward", false); // Values in parameters should be low case in the first letter because is variable name - 
@@ -1034,7 +1034,7 @@ public class EnemySystem : MonoBehaviour
 
             //Debug.Log("Player moved to left");
 
-            moveDirection = -1; // Setup new direction only once before to apply new position - 
+            moveDirection = 1; // Setup new direction only once before to apply new position - 
             enemyAnimator.SetBool("isBackward", true); // Values in parameters should be low case in the first letter because is variable name - 
             enemyAnimator.SetBool("isIdle", false); // Values in parameters should be low case in the first letter because is variable name - 
             enemyAnimator.SetBool("isForward", false); // Values in parameters should be low case in the first letter because is variable name - 
