519e949e1dc685bdd302e40897391acd3c6a31e5
ShadowOdyssey <ricardosolenoides@gmail.com>
2024-11-23 01:45:53 -0300

    Multiplayer Update

--- Assets/Database/logoffplayer.php
+++ Assets/Database/logoffplayer.php
@@ -8,7 +8,7 @@ $validateRequest = $_POST['validateRequest'];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "UPDATE lobby SET forward='no', backward='no', attack1='no', attack2='no', attack3='no', hit='no', health='100', profile='0', ready='no', duel='0', host='0' WHERE id = ?;";
+$sql = "UPDATE lobby SET forward='no', backward='no', attack1='no', attack2='no', attack3='no', hit='no', health='100', profile='0', ready='no', status='offline' duel='0', host='0' WHERE id = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $validateRequest);
 
